f5ffd765917f29bd638d003e3a2161b33ba7e175
Leighton Chen <leighton@asia.com>
2019-02-12 13:09:07 -0800

    Implement modal image navigation with image counter
    Displays current image number on top right of image. Can use back and
    forward buttons to navigate through images, and will loop back to
    beginning at end, and vice versa.

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   closeModal(event) {
     if (event.target.className.includes('modal')) {
       this.props.closeModal();
@@ -14,7 +10,12 @@ class Modal extends React.Component {
   render() {
     return (
       <div className="modal" style={{display: this.props.display}} onClick={this.closeModal.bind(this)}>
-        <img className="imgs" src={this.props.link} />
+        <button className="back" onClick={() => {this.props.btnBack(this.props.id)}}><span className="buttonText">&lt;</span></button>
+        <div className="content">
+          <div className="modal-counter">{this.props.id + 1} of {this.props.imageCount}</div>
+          <img className="imgs" src={this.props.link} />
+        </div>
+        <button className="forward" onClick={() => {this.props.btnNext(this.props.id)}}><span className="buttonText">&gt;</span></button>
       </div>
     )
   }
--- client/components/PhotoCarousel.jsx
+++ client/components/PhotoCarousel.jsx
@@ -15,7 +15,7 @@ class PhotoCarousel extends React.Component {
   }
 
   componentDidMount() {
-    fetch(`http://localhost:3333/api/photos/${this.props.id}`)
+    fetch(`http://localhost:3333/api/photos/${this.props.id}`) // $SERVER_URL
       .then(response => response.json())
       .then(links => {
         return links.map(({url}) => url)
@@ -36,15 +36,41 @@ class PhotoCarousel extends React.Component {
     });
   }
 
+  modalNavigateNext(id) {
+    id = (id + 1) % this.state.thumbnails.length;
+    this.setState({
+      modalId: id,
+    });
+  }
+
+  modalNavigateBack(id) {
+    id = ((id - 1) + this.state.thumbnails.length) % this.state.thumbnails.length;
+    this.setState({
+      modalId: id,
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="carousel-container">
           {this.state.thumbnails.map((link, id) => (
-            <PhotoTile link={link} id={id} openModal={this.openModal.bind(this)} />
+            <PhotoTile 
+              link={link} 
+              id={id} 
+              openModal={this.openModal.bind(this)} 
+            />
             ))}
         </div>
-        <Modal display={this.state.modalView} link={this.state.thumbnails[this.state.modalId]} closeModal={this.closeModal.bind(this)} />
+        <Modal 
+          display={this.state.modalView} 
+          link={this.state.thumbnails[this.state.modalId]} 
+          id={this.state.modalId}
+          imageCount={this.state.thumbnails.length}
+          closeModal={this.closeModal.bind(this)} 
+          btnBack={this.modalNavigateBack.bind(this)}
+          btnNext={this.modalNavigateNext.bind(this)}
+        />
       </div>
     )
   }
