0db82f6b5613be22366218a83192a661a672fb0c
Leighton Chen <leighton@asia.com>
2019-02-13 12:06:14 -0800

    Fix bug with seeding script

--- util/seeder.js
+++ util/seeder.js
@@ -92,20 +92,26 @@ mysql.createConnection({
               min: 1000,
               max: 30000,
             }),
-          }).save());
-          const numImages = faker.random.number({
-            min: 20,
-            max: 50,
-          });
-          for (let j = 0; j < numImages; j += 1) {
-            promises.push(Photo.build({
-              url: `https://s3-us-west-1.amazonaws.com/xillow-talk-photos/property_photos/sample${i + 1}.jpg`,
-              property_id: i + 1,
-            }).save());
-          }
+          }).save().catch(() => {}));
         }
         return Promise.all(promises)
-          .catch(() => {});
+          .catch((error) => {console.log(error)});
+      })
+      .then(() => {
+        // const promises = [];
+        // for (let i = 0; i < 100; i += 1) {
+        //   const numImages = faker.random.number({
+        //     min: 20,
+        //     max: 50,
+        //   });
+        //   for (let j = 0; j < numImages; j += 1) {
+        //     promises.push(Photo.build({
+        //       url: `https://s3-us-west-1.amazonaws.com/xillow-talk-photos/property_photos/sample${i + 1}.jpg`,
+        //       property_id: i + 1,
+        //     }).save().catch(() => {}));
+        //   }
+        // }
+        // return Promise.all(promises);
       })
       .then(() => sequelize.close());
   });
